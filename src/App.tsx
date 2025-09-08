48242666883b22728fc1b95ca44fdabc4a1e961b
Tudor James Florea <tudorjflorea@gmail.com>
2025-09-07 17:32:14 -0700

    starting client side added axios, api

--- src/App.tsx
+++ src/App.tsx
@@ -3,5 +3,10 @@ import { MantineProvider } from "@mantine/core";
 import { theme } from "./theme";
 
 export default function App() {
-  return <MantineProvider theme={theme}>App</MantineProvider>;
+  return <MantineProvider theme={theme}>
+
+
+
+    
+  </MantineProvider>;
 }
